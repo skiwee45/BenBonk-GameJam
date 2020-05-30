33e3dc27b7c4692efc54e6f5e3ac6265983742ec
CodeHacher123 <clashmineclans@gmail.com>
2020-05-30 12:28:43 -0400

    MainMenu&ContolMenu Started

---
+++ BenBonk Jam 1/Assets/SceneAssets/Game/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if(movement.x == -1 || movement.x == 1 || movement.y == -1 || movement.y == 1){
+            anim.SetBool("isRunning", true);
+        }
+        else{
+            anim.SetBool("isRunning", false);  
+		}
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+
+}
