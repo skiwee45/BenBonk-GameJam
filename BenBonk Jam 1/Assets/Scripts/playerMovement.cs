6f28eebf32e43d41b8d6d4356ea18735bd33f9f6
skiwee45 <raymond.lin.05@gmail.com>
2020-05-30 09:16:30 -0400

    Slingshot

---
+++ BenBonk Jam 1/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rigidbody; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
