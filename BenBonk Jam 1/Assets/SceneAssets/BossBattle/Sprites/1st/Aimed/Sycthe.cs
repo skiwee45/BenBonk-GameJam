596d00123e03212a5776a79ca1cdaf392084d071
CodeHacher123 <clashmineclans@gmail.com>
2020-05-31 17:16:39 -0400

    1ststagebosssetup

---
+++ BenBonk Jam 1/Assets/SceneAssets/BossBattle/Sprites/1st/Aimed/Sycthe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sycthe : MonoBehaviour
+{
+	public GameObject hitEffect;
+
+	private void OnTriggerEnter2D(Collider2D other){
+
+         if (other.gameObject.CompareTag("Player")) {
+            Debug.Log("Player is hit with scythe!");
+         }
+		 GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 5f);
+            Destroy(gameObject);
+
+	}
+
+}
