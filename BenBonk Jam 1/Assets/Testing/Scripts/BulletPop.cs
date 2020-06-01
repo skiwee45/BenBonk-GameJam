c4d7a80fa451b42c90a1002cb8f2b5c3e1b00ba0
skiwee45 <raymond.lin.05@gmail.com>
2020-06-01 08:17:29 -0400

    Revert "Merge branch 'master' of https://github.com/skiwee45/BenBonk-GameJam"
    This reverts commit b113490385930f07f205211084aaf6158898f094, reversing
    changes made to 4339552a0510f1f4e3da52d60bdc5a806a7ff3a2.

---
+++ BenBonk Jam 1/Assets/Testing/Scripts/BulletPop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BulletPop : MonoBehaviour
+{
+    public GameObject hitEffect;
+    //colliding script
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+            Debug.Log("bullet damage hit");
+        }
+        //particle effect and kills itself
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
