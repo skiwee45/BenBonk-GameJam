6a413d7d6e93c832ef55a7f56cf52c4781778fa6
CodeHacher123 <clashmineclans@gmail.com>
2020-06-06 10:35:19 -0400

    things done plz

---
+++ BenBonk Jam 1/Assets/Scenes/BossBattle/Sprites/GoToBos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToBos : MonoBehaviour
+{
+    public GameObject pb;
+    public GameObject boss;
+    public Transform reset;
+    public Transform player;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("win")){
+              pb.SetActive(false);
+              boss.SetActive(true);
+              player.position = reset.position;
+		}
+	}
+}
